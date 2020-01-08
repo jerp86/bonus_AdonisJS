f38ef4b17d9d377e3aa0551573234f341f4f4373
jerp86 <jerp4@hotmail.com>
2020-01-08 00:56:48 -0300

    Salvando relacionamentos

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -13,8 +13,11 @@ class UserController {
    */
   async store({ request }) {
     const data = request.only(['username', 'email', 'password']);
+    const addresses = request.input('addresses');
 
-    const user = await User.crreate(data);
+    const user = await User.create(data);
+
+    await user.addresses().createMany(addresses);
 
     return user;
   }
