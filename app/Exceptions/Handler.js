62b47bb0da8915a9593c31df3ca52a45ad3ec4be
jerp86 <jerp4@hotmail.com>
2020-01-08 00:36:18 -0300

    Configurando Sentry

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,6 +1,9 @@
+const Config = use('Config');
 const Env = use('Env');
 const Youch = use('youch');
 const BaseExceptionHandler = use('BaseExceptionHandler');
+
+const Sentry = require('@sentry/node');
 
 /**
  * This class handles all exceptions thrown during
@@ -46,7 +49,9 @@ class ExceptionHandler extends BaseExceptionHandler {
    * @return {void}
    */
   async report(error, { request }) {
-    console.log(error);
+    console.log(Config.get('services.sentry.dsn'));
+    Sentry.init({ dsn: Config.get('services.sentry.dsn') });
+    Sentry.captureException(error);
   }
 }
 
---
+++ config/services.js
@@ -0,0 +1,8 @@
+/** @typedef {import('@adonisjs/framework/src/Env')} Env */
+const Env = use('Env');
+
+module.exports = {
+  sentry: {
+    dsn: Env.get('SENTRY_DSN'),
+  },
+};
